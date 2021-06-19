51d6353c991e5ead269c2a44b773f0d91b08f61a
edwinsolisf12 <edwinsolisf12@gmail.com>
2021-06-19 11:04:36 -0400

    First commit: stm library files only

--- main.cpp
+++ main.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 
-#include "stm/math.h"
+//#include "stm/math.h"
 #include "stm/vector2.h"
 
 int main()
 {
-	stm::vec2f num1 = { 1.0f, 1.0f };
-	stm::vec2f num2(1.0f, 1.0f);
-	stm::vector<long long, 6> vec = { 1, 2, 3, 4, 5, 6 };
-
 	std::cout << "Hello world!\n";
 	std::cin.get();
 	return 0;
